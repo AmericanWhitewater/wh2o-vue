712eef4e03fc89df675642fa87dce40bb9fcfc0b
Drew <drew@drewalthage.com>
2020-02-18 10:49:00 -0700

    express server fix

--- server/server.js
+++ server/server.js
@@ -3,14 +3,11 @@
 const express = require("express");
 const serveStatic = require("serve-static");
 const path = require("path");
-const { checkIfDeployed } = require(path.join(
-  __dirname,
-  "../src/plugins/console-greeting"
-));
-const initCheck = checkIfDeployed();
 
 app = express();
+
 app.use(serveStatic(path.join(__dirname, "../dist")));
+
 const port = process.env.PORT || 3030;
-app.use(checkIfDeployed);
-app.listen(port, initCheck);
+
+app.listen(port);
