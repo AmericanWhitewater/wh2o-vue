17b583ad697e9eb1bf6833907bcb8a039bcc5356
Drew Althage <andrew.althage@gmail.com>
2020-03-16 20:19:35 -0600

    auth wip

--- src/app/plugins/carbon-icons.js
+++ src/app/plugins/carbon-icons.js
@@ -23,6 +23,7 @@ import Help32 from '@carbon/icons-vue/es/help/32.js'
 import Launch20 from '@carbon/icons-vue/es/launch/20.js'
 import List16 from '@carbon/icons-vue/es/list/16.js'
 import LogoFacebook24 from '@carbon/icons-vue/es/logo--facebook/24'
+import LogoFacebook32 from '@carbon/icons-vue/es/logo--facebook/32'
 import LogoLinkedIn24 from '@carbon/icons-vue/es/logo--linkedin/24'
 import Map32 from '@carbon/icons-vue/es/map/32.js'
 import Maximize16 from '@carbon/icons-vue/es/maximize/16.js'
@@ -61,6 +62,7 @@ Vue.use(CarbonIconsVue, {
     Launch20,
     List16,
     LogoFacebook24,
+    LogoFacebook32,
     LogoLinkedIn24,
     Map32,
     Maximize16,
--- src/app/views/user/components/access/user-forgot.vue
+++ src/app/views/user/components/access/user-forgot.vue
@@ -1,16 +1,42 @@
 <template>
   <div class="user-forgot">
-    forgot
+    <cv-form>
+      <cv-text-input
+        v-model="username"
+        class="mb-spacing-lg"
+        placeholder="Email Address"
+        type="email"
+        theme="light"
+      />
+      <cv-button
+        kind="primary"
+        small
+        class="mb-spacing-md"
+        :disabled="username.length < 3"
+        @click="submitForm"
+        v-text="'Submit'"
+      />
+    </cv-form>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'user-forgot'
+  name: 'user-forgot',
+  data: () => ({
+    username: ''
+  }),
+  methods: {
+    submitForm () {
+      // eslint-disable-next-line no-console
+      console.log('form submit')
+    }
+  }
 }
 </script>
 
 <style lang="scss">
 .user-forgot {
+
 }
 </style>
