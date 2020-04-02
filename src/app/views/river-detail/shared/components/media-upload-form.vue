6ba17946166515282c75725bb45b8210fe285d28
Drew Althage <andrew.althage@gmail.com>
2020-04-02 14:15:20 -0600

    media upload wip

--- src/app/views/river-detail/shared/components/media-upload-form.vue
+++ src/app/views/river-detail/shared/components/media-upload-form.vue
@@ -7,12 +7,12 @@
     />
     <cv-file-uploader
       ref="fileUploader"
-      v-model="formData.fileinput.file"
       data-modal-primary-focus
       helper-text="10mb max"
       accepts=".jpg,.png"
       class="mb-spacing-md"
       :disabled="formPending"
+      @change="setFile"
     />
     <cv-text-input
       v-model="formData.photo.author"
@@ -26,19 +26,17 @@
       v-model="formData.photo.subject"
       class="mb-spacing-md"
       label="Subject"
-      placeholder="label"
       :disabled="formPending"
     />
     <cv-text-input
       v-model="formData.photo.caption"
       class="mb-spacing-md"
       label="Caption"
-      placeholder="label"
       :disabled="formPending"
     />
     <cv-text-area
       v-model="formData.photo.description"
-      label="label"
+      label="Description"
       :class="{ 'mb-spacing-md': !parentIsModal }"
       :disabled="formPending"
     />
@@ -53,8 +51,7 @@
   </div>
 </template>
 <script>
-import { mapState } from 'vuex'
-import { globalAppActions } from '@/app/global/state'
+import { httpClient } from '@/app/global/services'
 export default {
   name: 'media-upload-form',
   props: {
@@ -72,15 +69,18 @@ export default {
     title: {
       type: String,
       required: false
+    },
+    rapids: {
+      type: Array,
+      required: false,
+      default: () => null
     }
   },
   data: () => ({
     formPending: false,
     formData: {
-      // camelCase it?
       fileinput: {
         file: null,
-        // what are these?
         section: '',
         section_id: ''
       },
@@ -98,10 +98,9 @@ export default {
   }),
 
   computed: {
-    ...mapState({
-      user: state => state.userState.userData.data,
-      rapids: state => state.riverDetailState.rapidsData.data
-    }),
+    user () {
+      return this.$store.state.userState.userData.data
+    },
     fieldsDisabled () {
       return !this.user || this.formPending
     },
@@ -115,34 +114,60 @@ export default {
     }
   },
   methods: {
-    formSubmit () {
-      alert('form submit')
-      this.$emit('form:submitted')
+    async submitForm () {
       this.formPending = true
-      // this.$store.state.dispatch(riverDetailActions.MEDIA_UPLOAD, this.formData)
-      const successful = true
-      setTimeout(() => {
-        this.formPending = false
-        if (successful) {
-          this.$emit('form:success')
-          this.$store.dispatch(globalAppActions.SEND_TOAST, {
-            title: 'Upload Successful',
-            kind: 'success',
-            override: true,
-            contrast: false,
-            action: false,
-            autoHide: true
-          })
-        } else {
-          this.$emit('form:error')
+      await httpClient.post('/graphql', {
+        query: `
+          mutation($file: Upload!) {
+            photoFileCreate(
+              fileinput: {
+                file: $file,
+                section: ${this.section.toUpperCase()},
+                section_id: "${0}"
+              },
+              photo: {
+                author: "${this.formData.photo.author}",
+                caption: "${this.formData.photo.caption}",
+                subject: "${this.formData.photo.subject}",
+                description: "${this.formData.photo.description}",
+                photo_date: "${this.todaysDate()}",
+                poi_id: "${this.formData.photo.poi_id}",
+                post_id: "${this.formData.photo.post_id}",
+                poi_name: "${this.formData.photo.poi_name}"
+              }
+            ) {
+              id
+            }
+          }`,
+        variables: {
+          file: this.formData.fileinput.file
         }
-      }, 3000)
+      }).then(r => {
+        this.formPending = false
+        // eslint-disable-next-line no-console
+        console.log('r :', r)
+      }).catch(e => {
+        this.formPending = false
+        // eslint-disable-next-line no-console
+        console.log('e :', e)
+      })
+    },
+    setFile (input) {
+      /** single file upload for now... */
+      this.formData.fileinput.file = input[0].file
+    },
+    todaysDate () {
+      const today = new Date()
+      return today.toISOString()
     }
   },
   created () {
     if (this.user && this.user.uname) {
       this.formData.photo.author = this.user.uname
     }
+  },
+  mounted () {
+    this.todaysDate()
   }
 }
 </script>
