bf98151cb9332fd8941259a4a16c800d1a2ff5d3
Nick Gottlieb <ngottlieb@gmail.com>
2020-08-20 13:06:15 -0700

    amend

--- src/app/views/river-detail/main-tab/components/recent-alerts/components/side-bar-articles.vue
+++ src/app/views/river-detail/main-tab/components/recent-alerts/components/side-bar-articles.vue
@@ -32,7 +32,7 @@
               class="mb-spacing-2xs"
               v-text="$titleCase(article.title)"
             />
-            <p v-html="article.abstract.slice(0, 150)" />
+            <p v-html="article.abstract" />
           </div>
         </div>
       </div>
