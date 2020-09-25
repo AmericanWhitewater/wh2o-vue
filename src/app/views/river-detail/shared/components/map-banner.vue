bd69710e5a13e9fd70b158b3868cb236d70aca64
Nick Gottlieb <ngottlieb@gmail.com>
2020-09-25 11:31:32 -0700

    use geojson instead of tileserver source for river detail main tab map banner

--- src/app/views/river-detail/shared/components/map-banner.vue
+++ src/app/views/river-detail/shared/components/map-banner.vue
@@ -2,17 +2,18 @@
   <div class="page-banner">
     <div class="map-wrapper">
       <template v-if="startingBounds">
-        <nwi-map
-          height="400"
-          :detail-reach-id="Number(reachId)"
-          :include-legend="false"
-          :has-controls="false"
-          :source-layers="$options.sourceLayers"
-          :starting-bounds="startingBounds"
-          fullscreen-target="map-wrapper"
-          hide-result-counter
-          static
-        />
+        <div
+          id="nwi-map-banner-container"
+        >
+          <div
+            id="nwi-map-banner"
+            ref="nwiMapBanner"
+            style="height:400px;"
+          />
+          <nwi-basemap-toggle
+            :offset-right="false"
+          />
+        </div>
       </template>
       <template v-else>
         <utility-block
@@ -27,39 +28,122 @@
 </template>
 <script>
 import bbox from '@turf/bbox'
-import { lineString } from '@turf/helpers'
-import NwiMap from '@/app/views/river-index/components/nwi-map.vue'
+import { lineString, point, featureCollection } from '@turf/helpers'
+import mapboxgl from 'mapbox-gl'
+import { mapState } from 'vuex'
+import NwiMapStyles from '@/app/views/river-index/components/nwi-map-styles'
+import {
+  mapboxAccessToken
+} from '@/app/environment'
+import { basemapToggleMixin } from '@/app/global/mixins'
 import UtilityBlock from '@/app/global/components/utility-block/utility-block.vue'
 
 export default {
   name: 'map-banner',
   components: {
-    'nwi-map': NwiMap,
     UtilityBlock
   },
+  mixins: [basemapToggleMixin],
   props: {
-    geom: {
-      type: String,
-      required: false
-    },
-    reachId: {
-      type: String,
-      required: false
-    },
     loading: {
       type: Boolean,
       required: false
     }
   },
-  sourceLayers: ['reach-segments', 'access'],
   computed: {
+    reachGeom () {
+      // TODO: get graphql API to return a linestring or geojson instead of this text
+      const geom = this.data?.geom?.split(',').map(d => d.split(' '))
+      return geom ? lineString(geom) : null
+    },
+    ...mapState({
+      mapStyle: state => state.riverIndexState.riverIndexData.mapStyle,
+      data: state => state.riverDetailState.riverDetailData.data
+    }),
     startingBounds () {
       // TODO: get graphql API to return a linestring or geojson instead of this text
-      if (this.geom) {
-        const bounds = this.geom.split(',').map(d => d.split(' '))
-        return bbox(lineString(bounds))
+      if (this.reachGeom) {
+        return bbox(this.reachGeom)
       }
       return null
+    }
+  },
+  methods: {
+    mountMap () {
+      mapboxgl.accessToken = mapboxAccessToken
+      const mapProps = {
+        container: this.$refs.nwiMapBanner,
+        style: this.baseMapUrl,
+        bounds: this.startingBounds,
+        fitBoundsOptions: { padding: 80 },
+        interactive: false
+      }
+
+      this.map = new mapboxgl.Map(mapProps)
+      this.map.on('styledata', this.loadReach)
+    },
+    loadReach () {
+      if (this.reachGeom) {
+        if (this.map.getSource('reachGeom') === undefined) {
+          this.map.addSource('reachGeom', {
+            type: 'geojson',
+            data: {
+              ...this.reachGeom
+            }
+          })
+        }
+        if (this.map.getLayer('reachGeom') === undefined) {
+          this.addReachToMap()
+          this.addAccessToMap()
+        }
+      }
+    },
+    addReachToMap () {
+      // this logic is duplicated in `nwi-map.vue` but seemed like it would
+      // confuse things to share, if it's used again anywhere else it should be
+      // refactored into a mixin or something
+      const colors = Object.values(NwiMapStyles.colorSchemes.difficulty)
+      const paintProps = {
+        ...NwiMapStyles.sourceLayers['reach-segments'].reachSegments.paint,
+        'line-color': [
+          'case',
+          ['>', ['get', 'difficulty'], 6],
+          colors[3],
+          ['>', ['get', 'difficulty'], 4],
+          colors[2],
+          ['>', ['get', 'difficulty'], 2],
+          colors[1],
+          ['<=', ['get', 'difficulty'], 2],
+          colors[0],
+          '#53789a'
+        ]
+      }
+      this.map.addLayer({
+        id: 'reachGeom',
+        source: 'reachGeom',
+        type: 'line',
+        layout: NwiMapStyles.sourceLayers['reach-segments'].reachSegments.layout,
+        paint: paintProps
+      })
+    },
+    addAccessToMap () {
+      const putin = point(this.reachGeom.geometry.coordinates[0])
+      putin.properties.type = 'put-in'
+      const takeout = point(this.reachGeom.geometry.coordinates.slice(-1)[0])
+      takeout.properties.type = 'take-out'
+      this.map.addSource('reachAccess', { type: 'geojson', data: featureCollection([putin, takeout]) })
+      this.map.addLayer({
+        id: 'reachAccess',
+        source: 'reachAccess',
+        type: 'circle',
+        layout: NwiMapStyles.sourceLayers.access.access.layout,
+        paint: NwiMapStyles.sourceLayers.access.access.paint
+      })
+    }
+  },
+  mounted () {
+    if (mapboxAccessToken) {
+      this.mountMap()
     }
   }
 }
