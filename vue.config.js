23945fa52e60495f174f7579541e8dc7a0c7c8eb
Ryan@AW <rgroth@americanwhitewater.org>
2020-10-19 09:33:50 -0400

    more ignored directories.

--- vue.config.js
+++ vue.config.js
@@ -4,18 +4,18 @@
  *
  */
 
-const dotenv = require('dotenv').config()
+const dotenv = require("dotenv").config();
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const path = require('path')
+const path = require("path");
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const ModuleDependencyWarning = require('webpack/lib/ModuleDependencyWarning')
+const ModuleDependencyWarning = require("webpack/lib/ModuleDependencyWarning");
 
 class IgnoreNotFoundExportPlugin {
-  apply (compiler) {
-    const messageRegExp = /export '.*'( \(reexported as '.*'\))? was not found in/
-    function doneHook (stats) {
+  apply(compiler) {
+    const messageRegExp = /export '.*'( \(reexported as '.*'\))? was not found in/;
+    function doneHook(stats) {
       stats.compilation.warnings = stats.compilation.warnings.filter(function (
         warn
       ) {
@@ -23,61 +23,62 @@ class IgnoreNotFoundExportPlugin {
           warn instanceof ModuleDependencyWarning &&
           messageRegExp.test(warn.message)
         ) {
-          return false
+          return false;
         }
-        return true
-      })
+        return true;
+      });
     }
     if (compiler.hooks) {
-      compiler.hooks.done.tap('IgnoreNotFoundExportPlugin', doneHook)
+      compiler.hooks.done.tap("IgnoreNotFoundExportPlugin", doneHook);
     } else {
-      compiler.plugin('done', doneHook)
+      compiler.plugin("done", doneHook);
     }
   }
 }
 
 // https://stackoverflow.com/questions/53431754/styling-not-applied-to-vue-web-component-during-development
 // ?? - if HMR - https://github.com/vuejs/vue-style-loader/pull/41/commits/c722f1cb5c2856a1197a13f04317506453c14add
-function enableShadowCss (config) {
+function enableShadowCss(config) {
   const configs = [
-    config.module.rule('vue').use('vue-loader'),
-    config.module.rule('css').oneOf('vue-modules').use('vue-style-loader'),
-    config.module.rule('css').oneOf('vue').use('vue-style-loader'),
-    config.module.rule('css').oneOf('normal-modules').use('vue-style-loader'),
-    config.module.rule('css').oneOf('normal').use('vue-style-loader'),
-    config.module.rule('postcss').oneOf('vue-modules').use('vue-style-loader'),
-    config.module.rule('postcss').oneOf('vue').use('vue-style-loader'),
+    config.module.rule("vue").use("vue-loader"),
+    config.module.rule("css").oneOf("vue-modules").use("vue-style-loader"),
+    config.module.rule("css").oneOf("vue").use("vue-style-loader"),
+    config.module.rule("css").oneOf("normal-modules").use("vue-style-loader"),
+    config.module.rule("css").oneOf("normal").use("vue-style-loader"),
+    config.module.rule("postcss").oneOf("vue-modules").use("vue-style-loader"),
+    config.module.rule("postcss").oneOf("vue").use("vue-style-loader"),
     config.module
-      .rule('postcss')
-      .oneOf('normal-modules')
-      .use('vue-style-loader'),
-    config.module.rule('postcss').oneOf('normal').use('vue-style-loader'),
-    config.module.rule('scss').oneOf('vue-modules').use('vue-style-loader'),
-    config.module.rule('scss').oneOf('vue').use('vue-style-loader'),
-    config.module.rule('scss').oneOf('normal-modules').use('vue-style-loader'),
-    config.module.rule('scss').oneOf('normal').use('vue-style-loader'),
-    config.module.rule('sass').oneOf('vue-modules').use('vue-style-loader'),
-    config.module.rule('sass').oneOf('vue').use('vue-style-loader'),
-    config.module.rule('sass').oneOf('normal-modules').use('vue-style-loader'),
-    config.module.rule('sass').oneOf('normal').use('vue-style-loader'),
-    config.module.rule('less').oneOf('vue-modules').use('vue-style-loader'),
-    config.module.rule('less').oneOf('vue').use('vue-style-loader'),
-    config.module.rule('less').oneOf('normal-modules').use('vue-style-loader'),
-    config.module.rule('less').oneOf('normal').use('vue-style-loader'),
-    config.module.rule('stylus').oneOf('vue-modules').use('vue-style-loader'),
-    config.module.rule('stylus').oneOf('vue').use('vue-style-loader'),
+      .rule("postcss")
+      .oneOf("normal-modules")
+      .use("vue-style-loader"),
+    config.module.rule("postcss").oneOf("normal").use("vue-style-loader"),
+    config.module.rule("scss").oneOf("vue-modules").use("vue-style-loader"),
+    config.module.rule("scss").oneOf("vue").use("vue-style-loader"),
+    config.module.rule("scss").oneOf("normal-modules").use("vue-style-loader"),
+    config.module.rule("scss").oneOf("normal").use("vue-style-loader"),
+    config.module.rule("sass").oneOf("vue-modules").use("vue-style-loader"),
+    config.module.rule("sass").oneOf("vue").use("vue-style-loader"),
+    config.module.rule("sass").oneOf("normal-modules").use("vue-style-loader"),
+    config.module.rule("sass").oneOf("normal").use("vue-style-loader"),
+
+    config.module.rule("less").oneOf("vue-modules").use("vue-style-loader"),
+    config.module.rule("less").oneOf("vue").use("vue-style-loader"),
+    config.module.rule("less").oneOf("normal-modules").use("vue-style-loader"),
+    config.module.rule("less").oneOf("normal").use("vue-style-loader"),
+    config.module.rule("stylus").oneOf("vue-modules").use("vue-style-loader"),
+    config.module.rule("stylus").oneOf("vue").use("vue-style-loader"),
     config.module
-      .rule('stylus')
-      .oneOf('normal-modules')
-      .use('vue-style-loader'),
-    config.module.rule('stylus').oneOf('normal').use('vue-style-loader')
-  ]
+      .rule("stylus")
+      .oneOf("normal-modules")
+      .use("vue-style-loader"),
+    config.module.rule("stylus").oneOf("normal").use("vue-style-loader"),
+  ];
   configs.forEach((c) =>
     c.tap((options) => {
-      options.shadowMode = true
-      return options
+      options.shadowMode = true;
+      return options;
     })
-  )
+  );
 }
 
 module.exports = {
@@ -95,8 +96,8 @@ module.exports = {
      */
     overlay: {
       error: false,
-      warning: false
-    }
+      warning: false,
+    },
   },
 
   css: {
@@ -106,9 +107,9 @@ module.exports = {
         @import '@/app/assets/scss/abstracts/_variables.scss'; 
         @import '@/app/assets/scss/abstracts/_mixins.scss';         
         @import '@/app/assets/scss/vendor/_carbon-components-helpers.scss';
-        `
-      }
-    }
+        `,
+      },
+    },
   },
 
   /**
@@ -117,60 +118,60 @@ module.exports = {
    *
    */
   pwa: {
-    name: 'American Whitewater',
-    themeColor: '#5a6872',
-    backgroundColor: '#537653',
-    msTileColor: '#FFFFFF',
+    name: "American Whitewater",
+    themeColor: "#5a6872",
+    backgroundColor: "#537653",
+    msTileColor: "#FFFFFF",
     assetsVersion: Math.floor(Math.random() * 1000000000),
-    appleMobileWebAppCapable: 'yes',
-    workboxPluginMode: 'InjectManifest',
+    appleMobileWebAppCapable: "yes",
+    workboxPluginMode: "InjectManifest",
     workboxOptions: {
-      swSrc: './src/sw.js',
-      swDest: 'service-worker.js'
+      swSrc: "./src/sw.js",
+      swDest: "service-worker.js",
     },
-    appleMobileWebAppStatusBarStyle: 'black'
+    appleMobileWebAppStatusBarStyle: "black",
   },
   chainWebpack: (config) => {
     config.module
-      .rule('file')
+      .rule("file")
       .test(/\.(png|mp4|jpe?g|gif)$/i)
-      .use('file-loader')
-      .loader('file-loader')
-      .end()
+      .use("file-loader")
+      .loader("file-loader")
+      .end();
 
     config
-      .plugin('IgnoreNotFoundExportPlugin')
-      .before('friendly-errors')
-      .use(IgnoreNotFoundExportPlugin)
+      .plugin("IgnoreNotFoundExportPlugin")
+      .before("friendly-errors")
+      .use(IgnoreNotFoundExportPlugin);
 
     config.resolve.alias.set(
-      'tinyqueue',
-      path.join(__dirname, '/node_modules/tinyqueue/tinyqueue.js')
-    )
+      "tinyqueue",
+      path.join(__dirname, "/node_modules/tinyqueue/tinyqueue.js")
+    );
 
     /**
      * Add scoped css support here.
      */
-    if (config.plugins.has('extract-css')) {
-      const extractCSSPlugin = config.plugin('extract-css')
+    if (config.plugins.has("extract-css")) {
+      const extractCSSPlugin = config.plugin("extract-css");
       extractCSSPlugin &&
         extractCSSPlugin.tap(() => [
           {
-            filename: 'build.css',
-            chunkFilename: 'build.css'
-          }
-        ])
+            filename: "build.css",
+            chunkFilename: "build.css",
+          },
+        ]);
     }
 
     if (process.env.VUE_APP_LARAVEL_DEPLOY) {
-      console.log('using shadow')
-      enableShadowCss(config)
+      console.log("using shadow");
+      enableShadowCss(config);
     }
   },
   configureWebpack: (config) => {
     config.optimization = {
-      minimize: process.env.NODE_ENV === 'production'
-    }
+      minimize: process.env.NODE_ENV === "production",
+    };
   },
-  publicPath: process.env.VUE_APP_BASE_URL || '/'
-}
+  publicPath: process.env.VUE_APP_BASE_URL || "/",
+};
