81cab767d2915e45f3d77a3e30827aaed1ac62dd
Drew <andrew.althage@gmail.com>
2020-11-07 16:11:50 -0700

    Current Flow Added to Flow Tab

--- src/app/views/river-detail/flow-tab/components/flow-stats/flow-stats.vue
+++ src/app/views/river-detail/flow-tab/components/flow-stats/flow-stats.vue
@@ -2,10 +2,22 @@
   <div class="flow-stats bx--row mb-spacing-md">
     <div class="bx--col">
       <h6 class="mb-spacing-2xs">
-        Avg
+        Current
       </h6>
-      <h3 v-if="!loading">
-        {{ avg || "n/a" }}
+      <h3 v-if="!loading && current">
+        {{ current || "n/a" }}
+      </h3>
+      <cv-skeleton-text
+        v-if="loading"
+        headline
+      />
+    </div>
+    <div class="bx--col">
+      <h6 class="mb-spacing-2xs">
+        Average
+      </h6>
+      <h3 v-if="!loading && stats">
+        {{ stats.avg || "n/a" }}
       </h3>
       <cv-skeleton-text
         v-if="loading"
@@ -16,8 +28,8 @@
       <h6 class="mb-spacing-2xs">
         High
       </h6>
-      <h3 v-if="!loading">
-        {{ max || "n/a" }}
+      <h3 v-if="!loading && stats">
+        {{ stats.max || "n/a" }}
       </h3>
       <cv-skeleton-text
         v-if="loading"
@@ -28,8 +40,8 @@
       <h6 class="mb-spacing-2xs">
         Low
       </h6>
-      <h3 v-if="!loading">
-        {{ min || "n/a" }}
+      <h3 v-if="!loading && stats">
+        {{ stats.min || "n/a" }}
       </h3>
       <cv-skeleton-text
         v-if="loading"
@@ -39,14 +51,8 @@
   </div>
 </template>
 <script>
-import { formatStats } from './format-stats'
 export default {
   name: 'flow-stats',
-  setup (props) {
-    const { min, avg, max } = formatStats(props.readings)
-
-    return { min, avg, max }
-  },
   props: {
     loading: {
       type: Boolean,
@@ -56,8 +62,33 @@ export default {
       type: Array,
       required: true,
       default: () => []
+    },
+    current: {
+      type: Number,
+      required: false,
+      default: 0
+    }
+  },
+  computed: {
+    stats () {
+      if (this.readings.length) {
+        let readingsSum = 0
+        const data = this.readings.map((reading) =>
+          Math.floor(reading.reading)
+        )
+
+        data.forEach((reading) => {
+          readingsSum = readingsSum + reading
+        })
+
+        return {
+          min: Math.min(...data),
+          max: Math.max(...data),
+          avg: Math.floor(readingsSum / this.readings.length)
+        }
+      }
+      return null
     }
   }
-
 }
 </script>
