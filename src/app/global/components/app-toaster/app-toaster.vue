9d09d7f426c1a00bbf2f0690ec782c9ba0fc4f94
Drew <andrew.althage@gmail.com>
2020-11-22 16:41:36 -0700

    Toast Timeout Clear

--- src/app/global/components/app-toaster/app-toaster.vue
+++ src/app/global/components/app-toaster/app-toaster.vue
@@ -67,7 +67,7 @@ export default {
           this.$store.dispatch("Global/closeToast", 0);
         }, 5000);
       } else {
-        clearInterval(closeTimer);
+        clearTimeout(closeTimer);
       }
     },
     updateAvailable(update) {
