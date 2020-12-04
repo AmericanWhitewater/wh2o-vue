98a22e321fcc905614e904cb203f3ec1692bdee4
Drew <andrew.althage@gmail.com>
2020-12-04 16:10:39 -0700

    Cleanup

--- src/app/views/river-detail/river-creator.vue
+++ src/app/views/river-detail/river-creator.vue
@@ -2,9 +2,7 @@
   <div class="river-creator">
     <div class="bx--grid">
       <div class="bx--row">
-        <div
-          class="bx--col-sm-16 bx--col-lg-10 map-column"
-        >
+        <div class="bx--col-sm-16 bx--col-lg-10 map-column">
           <geometry-editor @updatedGeom="updateReachGeom" />
         </div>
         <div class="bx--col-sm-16 bx--col-lg-6 form-fields-column">
@@ -24,13 +22,9 @@
             label="Section"
             class="mb-spacing-md"
           />
-          <cv-select
-            v-model="reach.class"
-            label="Class"
-            class="mb-spacing-md"
-          >
+          <cv-select v-model="reach.class" label="Class" class="mb-spacing-md">
             <cv-select-option
-              v-for="item in $options.reachClasses"
+              v-for="item in reachClasses"
               :key="item"
               :value="item"
             >
@@ -39,10 +33,11 @@
           </cv-select>
           <label for="Content Editor" class="bx--label">Description</label>
           <content-editor
-              :content="!reach.description ? ' ' : reach.description"
-              show-control-bar
-              @content:updated="reach.description = $event" />
-          <div class="mb-spacing-md mt-spacing-md"/>
+            :content="!reach.description ? ' ' : reach.description"
+            show-control-bar
+            @content:updated="reach.description = $event"
+          />
+          <div class="mb-spacing-md mt-spacing-md" />
           <cv-link
             @click="additionalFieldsVisible = !additionalFieldsVisible"
             @keydown.enter="additionalFieldsVisible = !additionalFieldsVisible"
@@ -51,7 +46,7 @@
               `${additionalFieldsVisible ? "Hide" : "Show"} Additional Fields`
             }}
           </cv-link>
-          <div class="mb-spacing-lg"/>
+          <div class="mb-spacing-lg" />
           <template v-if="additionalFieldsVisible">
             <cv-text-input
               v-model="reach.agency"
@@ -89,119 +84,35 @@
               class="mb-spacing-md"
             />
             <cv-text-input v-model="reach.maxgradient" label="Max Gradient" />
-            <div class="mb-spacing-lg"/>
+            <div class="mb-spacing-lg" />
           </template>
           <cv-button-set>
-          <cv-button
-            :disabled="createLoading"
-            @click.exact="handleSubmit"
-            @keydown.enter="handleSubmit"
-          >
-            Submit
-          </cv-button>
+            <cv-button
+              :disabled="createLoading"
+              @click.exact="handleSubmit"
+              @keydown.enter="handleSubmit"
+            >
+              Submit
+            </cv-button>
           </cv-button-set>
         </div>
-        
       </div>
     </div>
   </div>
 </template>
 <script>
 import { createReach } from "@/app/services";
-import turfLength from '@turf/length'
-import GeometryEditor from "@/app/views/river-detail/components/geometry-edit-modal/components/geometry-editor.vue"
-import ContentEditor from '@/app/global/components/content-editor/content-editor.vue'
+import { reachClasses } from "@/app/global/mixins";
+import turfLength from "@turf/length";
+import GeometryEditor from "@/app/views/river-detail/components/geometry-edit-modal/components/geometry-editor.vue";
+import ContentEditor from "@/app/global/components/content-editor/content-editor.vue";
 export default {
   name: "river-creator",
   components: {
     GeometryEditor,
-    ContentEditor
+    ContentEditor,
   },
-  /* eslint-disable-next-line max-len */
-  reachClasses: [
-    "none",
-    "I",
-    "I(II)",
-    "I(III)",
-    "I(IV)",
-    "I(V)",
-    "I(V+)",
-    "I-II",
-    "I-II(III)",
-    "I-II(IV)",
-    "I-II(V)",
-    "I-II(V+)",
-    "II",
-    "II(III)",
-    "II(IV)",
-    "II(V)",
-    "II(V+)",
-    "I-II+(III)",
-    "I-II+(IV)",
-    "I-II+(V)",
-    "I-II+(V+)",
-    "II+",
-    "II+(III)",
-    "II+(IV)",
-    "II+(V)",
-    "II+(V+)",
-    "I-III",
-    "I-III(IV)",
-    "I-III(V)",
-    "I-III(V+)",
-    "II-III",
-    "II-III(IV)",
-    "II-III(V)",
-    "II-III(V+)",
-    "III",
-    "I-III+",
-    "I-III+(IV)",
-    "I-III+(V)",
-    "I-III+(V+)",
-    "II-III+",
-    "II-III+(IV)",
-    "II-III+(V)",
-    "II-III+(V+)",
-    "III+",
-    "III+(IV)",
-    "III+(V)",
-    "III+(V+)",
-    "I-IV",
-    "I-IV(V)",
-    "I-IV(V+)",
-    "II-IV",
-    "II-IV(V)",
-    "II-IV(V+)",
-    "III-IV",
-    "III-IV(V)",
-    "III-IV(V+)",
-    "IV",
-    "IV(V)",
-    "IV(V+)",
-    "I-IV+",
-    "I-IV+(V)",
-    "I-IV+(V+)",
-    "II-IV+",
-    "II-IV+(V)",
-    "II-IV+(V+)",
-    "III-IV+",
-    "III-IV+(V)",
-    "III-IV+(V+)",
-    "IV+",
-    "I-V",
-    "I-V(V+)",
-    "II-V",
-    "II-V(V+)",
-    "III-V",
-    "III-V(V+)",
-    "IV-V",
-    "IV-V(V+)",
-    "V",
-    "II-V+",
-    "III-V+",
-    "IV-V+",
-    "V+",
-  ],
+  mixins: [reachClasses],
   data: () => ({
     geom: null,
     createLoading: false,
@@ -221,37 +132,36 @@ export default {
       river: "",
       section: "",
       tloc: "",
-      zipcode: ""
+      zipcode: "",
     },
   }),
   computed: {
-    reachLength () {
-          if (this.geom) {
-            return Number(turfLength(this.geom, { units: 'miles' }).toPrecision(2))
-          } else {
-            return undefined
-          }
-        },
+    reachLength() {
+      if (this.geom) {
+        return Number(turfLength(this.geom, { units: "miles" }).toPrecision(2));
+      } else {
+        return undefined;
+      }
+    },
   },
   methods: {
     updateReachGeom(val) {
-      this.geom = val
-      
-      this.reach.geom = val.geometry
-      this.reach.ploc = val.geometry.coordinates[0]
-      this.reach.tloc = val.geometry.coordinates.slice(-1)[0]
-      this.reach.length = this.reachLength
+      this.geom = val;
+
+      this.reach.geom = val.geometry;
+      this.reach.ploc = val.geometry.coordinates[0];
+      this.reach.tloc = val.geometry.coordinates.slice(-1)[0];
+      this.reach.length = this.reachLength;
     },
     async handleSubmit() {
       try {
         this.createLoading = true;
         const result = await createReach({
           id: this.$randomId(),
-          reach: this.reach
+          reach: this.reach,
         });
 
         if (!result.errors) {
-
           this.$store.dispatch("Global/sendToast", {
             kind: "success",
             title: "Success",
@@ -266,7 +176,6 @@ export default {
           });
         }
       } catch (error) {
-        
         this.$store.dispatch("Global/sendToast", {
           kind: "error",
           title: "Error",
@@ -278,7 +187,7 @@ export default {
     },
   },
   created() {
-    this.$store.dispatch('RiverDetail/dataReset')
-  }
+    this.$store.dispatch("RiverDetail/dataReset");
+  },
 };
 </script>
