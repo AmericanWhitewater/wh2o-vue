6fcf75ed15b92f6af8e371ae5d7409001ce04798
Drew <andrew.althage@gmail.com>
2020-12-07 17:59:37 -0700

    Wait for user data

--- src/app/views/river-detail/river-creator.vue
+++ src/app/views/river-detail/river-creator.vue
@@ -1,102 +1,124 @@
 <template>
   <div class="river-creator">
     <div class="bx--grid">
-      <div class="bx--row">
-        <div class="bx--col-sm-16 bx--col-lg-10 map-column">
-          <geometry-editor @updatedGeom="updateReachGeom" />
+      <template v-if="userLoading">
+        <div class="bx--row">
+          <div class="bx--col">
+            <utility-block state="loading" hide-text />
+          </div>
         </div>
-        <div class="bx--col-sm-16 bx--col-lg-6 form-fields-column">
-          <h1 class="mb-sm mt-spacing-sm">New Reach</h1>
-          <cv-text-input
-            v-model="reach.river"
-            label="River"
-            class="mb-spacing-md"
-          />
-          <cv-text-input
-            v-model="reach.altname"
-            label="Alt River Name"
-            class="mb-spacing-md"
-          />
-          <cv-text-input
-            v-model="reach.section"
-            label="Section"
-            class="mb-spacing-md"
-          />
-          <cv-select v-model="reach.class" label="Class" class="mb-spacing-md">
-            <cv-select-option
-              v-for="item in reachClasses"
-              :key="item"
-              :value="item"
+      </template>
+      <template v-else-if="user && canCreate">
+        <div class="bx--row">
+          <div class="bx--col-sm-16 bx--col-lg-10 map-column">
+            <geometry-editor @updatedGeom="updateReachGeom" />
+          </div>
+          <div class="bx--col-sm-16 bx--col-lg-6 form-fields-column">
+            <h1 class="mb-sm mt-spacing-sm">New Reach</h1>
+            <cv-text-input
+              v-model="reach.river"
+              label="River"
+              class="mb-spacing-md"
+            />
+            <cv-text-input
+              v-model="reach.altname"
+              label="Alt River Name"
+              class="mb-spacing-md"
+            />
+            <cv-text-input
+              v-model="reach.section"
+              label="Section"
+              class="mb-spacing-md"
+            />
+            <cv-select
+              v-model="reach.class"
+              label="Class"
+              class="mb-spacing-md"
             >
-              {{ item }}
-            </cv-select-option>
-          </cv-select>
-          <label for="Content Editor" class="bx--label">Description</label>
-          <content-editor
-            :content="!reach.description ? ' ' : reach.description"
-            show-control-bar
-            @content:updated="reach.description = $event"
-          />
-          <div class="mb-spacing-md mt-spacing-md" />
-          <cv-link
-            @click="additionalFieldsVisible = !additionalFieldsVisible"
-            @keydown.enter="additionalFieldsVisible = !additionalFieldsVisible"
-          >
-            {{
-              `${additionalFieldsVisible ? "Hide" : "Show"} Additional Fields`
-            }}
-          </cv-link>
-          <div class="mb-spacing-lg" />
-          <template v-if="additionalFieldsVisible">
-            <cv-text-input
-              v-model="reach.agency"
-              label="Agency"
-              class="mb-spacing-md"
+              <cv-select-option
+                v-for="item in reachClasses"
+                :key="item"
+                :value="item"
+              >
+                {{ item }}
+              </cv-select-option>
+            </cv-select>
+            <label for="Content Editor" class="bx--label">Description</label>
+            <content-editor
+              :content="!reach.description ? ' ' : reach.description"
+              show-control-bar
+              @content:updated="reach.description = $event"
             />
-            <cv-text-input
-              v-model="reach.county"
-              label="County"
-              class="mb-spacing-md"
-            />
-            <cv-text-input
-              v-model="reach.zipcode"
-              label="Zipcode"
-              class="mb-spacing-md"
-            />
-            <cv-text-input
-              v-model="reach.length"
-              label="Length"
-              class="mb-spacing-md"
-            />
-            <cv-text-input
-              v-model="reach.permiturl"
-              label="Permit URL"
-              class="mb-spacing-md"
-            />
-            <cv-text-input
-              v-model="reach.permitinfo"
-              label="Permit Info"
-              class="mb-spacing-md"
-            />
-            <cv-text-input
-              v-model="reach.avggradient"
-              label="Average Gradient"
-              class="mb-spacing-md"
-            />
-            <cv-text-input v-model="reach.maxgradient" label="Max Gradient" />
+            <div class="mb-spacing-md mt-spacing-md" />
+            <cv-link
+              @click="additionalFieldsVisible = !additionalFieldsVisible"
+              @keydown.enter="
+                additionalFieldsVisible = !additionalFieldsVisible
+              "
+            >
+              {{
+                `${additionalFieldsVisible ? "Hide" : "Show"} Additional Fields`
+              }}
+            </cv-link>
             <div class="mb-spacing-lg" />
-          </template>
-          <cv-button-set>
-            <cv-button
-              :disabled="createLoading"
-              @click.exact="handleSubmit"
-              @keydown.enter="handleSubmit"
-            >
-              Submit
-            </cv-button>
-          </cv-button-set>
+            <template v-if="additionalFieldsVisible">
+              <cv-text-input
+                v-model="reach.agency"
+                label="Agency"
+                class="mb-spacing-md"
+              />
+              <cv-text-input
+                v-model="reach.county"
+                label="County"
+                class="mb-spacing-md"
+              />
+              <cv-text-input
+                v-model="reach.zipcode"
+                label="Zipcode"
+                class="mb-spacing-md"
+              />
+              <cv-text-input
+                v-model="reach.length"
+                label="Length"
+                class="mb-spacing-md"
+              />
+              <cv-text-input
+                v-model="reach.permiturl"
+                label="Permit URL"
+                class="mb-spacing-md"
+              />
+              <cv-text-input
+                v-model="reach.permitinfo"
+                label="Permit Info"
+                class="mb-spacing-md"
+              />
+              <cv-text-input
+                v-model="reach.avggradient"
+                label="Average Gradient"
+                class="mb-spacing-md"
+              />
+              <cv-text-input v-model="reach.maxgradient" label="Max Gradient" />
+              <div class="mb-spacing-lg" />
+            </template>
+            <cv-button-set>
+              <cv-button
+                :disabled="createLoading"
+                @click.exact="handleSubmit"
+                @keydown.enter="handleSubmit"
+              >
+                Submit
+              </cv-button>
+            </cv-button-set>
+          </div>
         </div>
-      </div>
+      </template>
+      <template v-else>
+        <div class="bx--row">
+          <div class="bx--col">
+            <utility-block state="error" text="Unauthorized" />
+          </div>
+        </div>
+      </template>
     </div>
   </div>
 </template>
@@ -106,11 +128,14 @@ import { reachClasses } from "@/app/global/mixins";
 import turfLength from "@turf/length";
 import GeometryEditor from "@/app/views/river-detail/components/geometry-edit-modal/components/geometry-editor.vue";
 import ContentEditor from "@/app/global/components/content-editor/content-editor.vue";
+import { mapState } from "vuex";
+import UtilityBlock from "@/app/global/components/utility-block/utility-block.vue";
 export default {
   name: "river-creator",
   components: {
     GeometryEditor,
     ContentEditor,
+    UtilityBlock,
   },
   mixins: [reachClasses],
   data: () => ({
@@ -136,15 +161,16 @@ export default {
     },
   }),
   computed: {
+    ...mapState({
+      userLoading: (state) => state.User.loading,
+      user: (state) => state.User.data,
+    }),
     reachLength() {
       if (this.geom) {
         return Number(turfLength(this.geom, { units: "miles" }).toPrecision(2));
       } else {
         return undefined;
       }
-    },
-    user() {
-      return this.$store.state.User.data
     },
     canCreate() {
       if (this.user) {
@@ -204,14 +230,5 @@ export default {
   created() {
     this.$store.dispatch("RiverDetail/dataReset");
   },
-  mounted() {
-    if(!this.canCreate) {
-      this.$router.push('/')
-      this.$store.dispatch('Global/sendToast', {
-        kind: 'info',
-        title: 'Unauthorized'
-      })
-    }
-  }
 };
 </script>
