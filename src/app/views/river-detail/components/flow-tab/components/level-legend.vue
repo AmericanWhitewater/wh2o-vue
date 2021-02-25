7d40073204a81e98ba82215c7644870f57445a7f
Ryan@AW <rgroth@americanwhitewater.org>
2021-02-24 20:51:36 -0500

    started getting range to look OK.

--- src/app/views/river-detail/components/flow-tab/components/level-legend.vue
+++ src/app/views/river-detail/components/flow-tab/components/level-legend.vue
@@ -1,100 +1,115 @@
 <!--
 takes a range[] (see getEmptyRange for model)
 -->
-<template functional>
+<template >
+  <div>
   <div class="level-legend">
     <h6 class="mb-spacing-sm">Level Legend</h6>
     <ul>
       <li
-        v-for="(item, index) in $options.legendItems"
-        :key="index"
-        class="bx--type-caption"
+          v-for="(item, index) in $options.legendItems"
+          :key="index"
+          class="bx--type-caption"
       >
-        <svg
-          :class="item.color_class"
-          xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
-          viewBox="0 0 16 16"
-        >
-          <g fill="">
-            <rect width="16" height="16" rx="1" ry="1" fill="" />
-          </g>
-        </svg>
+        <range-box :color-class="item.color_class"/>
         {{ item.label }}
       </li>
     </ul>
   </div>
+  <div>
+    <ul>
+      <li
+          v-for="val in enumeratedRanges"
+          :key="val.value"
+      >
+        <range-box :color-class="val.min_class"/>
+        {{ val.range.min }}
+        <range-box :color-class="val.max_class"/>
+        {{ val.range.max}}
+      </li>
+    </ul>
+  </div>
+    </div>
 </template>
 <script>
-export default {
-  name: "level-legend",
-  props:{
-    ranges: {type: Array, required:false, default:()=>[]}
-  },
-  computed:{
-    function rangetoclass(range){
-      switch(range[0])
-      {
-        case 'R':
-          switch(range[1])
-        {
-          case '0':
-          case '1':
-          case '2':
-            return ('low-runnable')
-          case '3':
-            case '4':
-              case '5':
-                return('runnable')
-          case '6':
-            case '7':
-              case '8':
-                return('high-runnable')
+import RangeBox from './range-box'
 
+
+export default {
+  name: 'level-legend',
+  components: { RangeBox },
+  props: {
+    ranges: {
+      type: Array,
+      required: false,
+      default: () => []
+    }
+  },
+  computed: {
+
+    enumeratedRanges: function () {
+      function rangetoclass (range) {
+        switch (range[0]) {
+          case 'R':
+            switch (range[1]) {
+              case '0':
+              case '1':
+              case '2':
+                return ('low-runnable')
+              case '3':
+              case '4':
+              case '5':
+                return ('runnable')
+              case '6':
+              case '7':
+              case '8':
+                return ('high-runnable')
+
+            }
+            return 'runnable'
+          case 'H':
+            return 'above-recommended'
+          case 'L':
+            return 'below-recommended'
 
         }
-          return 'runnable';
-        case 'H':
-          return 'above-recommended'
-        case 'L':
-          return 'below-recommended'
-
       }
+
+      const mins = x => ({
+        min_class: rangetoclass(x.range_min),
+        max_class: rangetoclass(x.range_max),
+        range: x
+      })
+
+      return [...this.ranges.map(mins)].sort((x, y) => x.value - y.value)
     }
-    const x=>({class:rangetoclass(x.range_min),value:x.min,range:x});
-
-
-    const x = [
-        ...this.ranges.map(x=>,...)
-
   },
   legendItems: [
     {
-      label: "Above Recommended",
-      color_class: "above-recommended",
+      label: 'Above Recommended',
+      color_class: 'above-recommended',
     },
     {
-      label: "High Runnable",
-      color_class: "high-runnable",
+      label: 'High Runnable',
+      color_class: 'high-runnable',
     },
     {
-      label: "Runnable",
-      color_class: "runnable",
+      label: 'Runnable',
+      color_class: 'runnable',
     },
     {
-      label: "Low Runnable",
-      color_class: "low-runnable",
+      label: 'Low Runnable',
+      color_class: 'low-runnable',
     },
     {
-      label: "Below Recommended",
-      color_class: "below-recommended",
+      label: 'Below Recommended',
+      color_class: 'below-recommended',
     },
   ],
-};
+}
 </script>
 <style lang="scss" scoped>
-.level-legend {
+
 
   ul {
 
@@ -131,5 +146,4 @@ export default {
       }
     }
   }
-}
 </style>
