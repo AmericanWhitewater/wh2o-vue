897da9652d8130f3a1525e8fc02496641cc952f2
Nick Gottlieb <ngottlieb@gmail.com>
2021-04-20 12:04:20 -0700

    refactor media-upload-form into media-upload-modal

--- src/app/views/river-detail/components/index.js
+++ src/app/views/river-detail/components/index.js
@@ -1,7 +1,6 @@
 export { default as AlertUpdateModal } from './alert-update-modal.vue'
 export { default as ImageGallery } from './image-gallery/image-gallery.vue'
 export { default as MapBanner } from './map-banner.vue'
-export { default as MediaUploadForm } from './media-upload-form.vue'
 export { default as MediaUploadModal } from './media-upload-modal.vue'
 export { default as ReachTitleEditModal } from './reach-title-edit-modal.vue'
 
