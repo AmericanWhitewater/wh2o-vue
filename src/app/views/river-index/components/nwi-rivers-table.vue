b332f677276a4bfaf1559629a063a0881f7a14f4
Nick Gottlieb <ngottlieb@gmail.com>
2021-05-12 08:14:59 -0700

    lint nwi-rivers-table

--- src/app/views/river-index/components/nwi-rivers-table.vue
+++ src/app/views/river-index/components/nwi-rivers-table.vue
@@ -1,21 +1,16 @@
 <template>
-  <div
-    id="nwi-rivers-table"
-    kind="standard"
-  >
+  <div id="nwi-rivers-table" kind="standard">
     <template v-if="searchLoading">
       <utility-block state="loading" />
     </template>
     <template v-else-if="reaches && reaches.length > 0">
-      <div
-        class="bx--data-table-container river-index"
-      >
+      <div class="bx--data-table-container river-index">
         <table class="bx--data-table river-table">
           <thead>
             <tr>
               <th>
                 <strong>Name</strong>
-                <br>Section
+                <br >Section
               </th>
               <th>Level</th>
               <th>&nbsp;</th>
@@ -27,12 +22,19 @@
                 v-for="reach in reaches"
                 :key="reach.properties.id"
                 :ref="`reach-${reach.properties.id}`"
-                :class="{ active: mouseoveredFeature && reach.properties.id === mouseoveredFeature.properties.id}"
+                :class="{
+                  active:
+                    mouseoveredFeature &&
+                    reach.properties.id === mouseoveredFeature.properties.id,
+                }"
                 @mouseover="debouncedMouseover(reach)"
                 @mouseleave="debouncedMouseover()"
               >
                 <td
-                  :class="[`${reach.properties.condition}`, 'river-name-section']"
+                  :class="[
+                    `${reach.properties.condition}`,
+                    'river-name-section',
+                  ]"
                 >
                   <a :href="`/river-detail/${reach.properties.id}/main`">
                     <strong>{{ displayReachTitle(reach) }}</strong>
@@ -70,160 +72,164 @@
         />
       </template>
     </template>
-    <template v-else>
-      Error
-    </template>
+    <template v-else> Error </template>
   </div>
 </template>
 
 <script>
-import { lineString } from '@turf/helpers'
-import debounce from 'lodash.debounce'
-import ZoomIn16 from '@carbon/icons-vue/lib/zoom--in/16'
-import scrollIntoView from 'scroll-into-view-if-needed'
-import { Breakpoints } from '@/app/global/services'
-import { mapState } from 'vuex'
-import UtilityBlock from '@/app/global/components/utility-block/utility-block.vue'
+import { lineString } from "@turf/helpers";
+import debounce from "lodash.debounce";
+import ZoomIn16 from "@carbon/icons-vue/lib/zoom--in/16";
+import scrollIntoView from "scroll-into-view-if-needed";
+import { Breakpoints } from "@/app/global/services";
+import { mapState } from "vuex";
+import UtilityBlock from "@/app/global/components/utility-block/utility-block.vue";
 export default {
-  name: 'nwi-rivers-table',
+  name: "nwi-rivers-table",
   components: {
     ZoomIn16,
-    UtilityBlock
+    UtilityBlock,
   },
   props: {
     reachesOnMap: {
       type: Array,
       required: false,
-      default: () => []
-    }
+      default: () => [],
+    },
   },
   data: () => ({
     windowWidth: 0,
-    mq: Breakpoints
+    mq: Breakpoints,
   }),
   computed: {
     ...mapState({
-      searchResults: state => state.RiverIndex.mapSearchResults,
-      searchTerm: state => state.RiverIndex.mapSearchTerm,
-      searchLoading: state => state.RiverIndex.mapSearchLoading,
-      mouseoveredFeature: state => state.RiverIndex.mouseoveredFeature
+      searchResults: (state) => state.RiverIndex.mapSearchResults,
+      searchTerm: (state) => state.RiverIndex.mapSearchTerm,
+      searchLoading: (state) => state.RiverIndex.mapSearchLoading,
+      mouseoveredFeature: (state) => state.RiverIndex.mouseoveredFeature,
     }),
-    showingSearchResults () {
-      return Boolean(this.searchTerm)
+    showingSearchResults() {
+      return Boolean(this.searchTerm);
     },
-    reaches () {
+    reaches() {
       if (this.showingSearchResults) {
         if (this.searchResults && this.searchResults.length) {
-          return this.searchResults.map(x => this.convertSearchResponseToGeoJSON(x))
+          return this.searchResults.map((x) =>
+            this.convertSearchResponseToGeoJSON(x)
+          );
         }
-        return []
+        return [];
       }
       // alphabetize results if they're just displaying from the map
       const alphabetizedReaches = this.reachesOnMap.slice(0).sort((a, b) => {
-        const nameA = a.properties.river.toUpperCase()
-        const nameB = b.properties.river.toUpperCase()
+        const nameA = a.properties.river.toUpperCase();
+        const nameB = b.properties.river.toUpperCase();
 
         if (nameA < nameB) {
-          return -1
+          return -1;
         }
         if (nameA > nameB) {
-          return 1
+          return 1;
         }
 
         // names must be equal
-        return 0
-      })
-      return alphabetizedReaches
+        return 0;
+      });
+      return alphabetizedReaches;
     },
-    noReaches () {
-      return Boolean(this.reaches && this.reaches.length === 0)
+    noReaches() {
+      return Boolean(this.reaches && this.reaches.length === 0);
     },
-    columns () {
-      return ['Reach', 'Difficulty', 'Flow', 'Zoom']
+    columns() {
+      return ["Reach", "Difficulty", "Flow", "Zoom"];
     },
-    rowSize () {
+    rowSize() {
       if (this.windowWidth < this.mq.lg) {
-        return 'compact'
+        return "compact";
       }
-      return 'standard'
-    }
-
+      return "standard";
+    },
   },
   watch: {
-    mouseoveredFeature (feature) {
+    mouseoveredFeature(feature) {
       if (feature) {
-        const reachId = feature.properties.id
+        const reachId = feature.properties.id;
         if (
           this.$refs[`reach-${reachId}`] &&
           this.$refs[`reach-${reachId}`].length > 0
         ) {
           scrollIntoView(this.$refs[`reach-${reachId}`][0], {
-            scrollMode: 'if-needed',
-            block: 'center',
-            inline: 'nearest',
-            behavior: 'smooth',
-            boundary: this.$el
-          })
+            scrollMode: "if-needed",
+            block: "center",
+            inline: "nearest",
+            behavior: "smooth",
+            boundary: this.$el,
+          });
         }
       }
-    }
+    },
   },
   methods: {
-    displayReachTitle (reach) {
-      return [reach.properties.river, reach.properties.class].join(' - ')
+    displayReachTitle(reach) {
+      return [reach.properties.river, reach.properties.class].join(" - ");
     },
-    displayGaugeReading (reach) {
+    displayGaugeReading(reach) {
       if (reach.properties.gage_0_reading) {
-        return [parseFloat(reach.properties.gage_0_reading.toFixed(2)), reach.properties.gage_0_unit].join(' ')
+        return [
+          parseFloat(reach.properties.gage_0_reading.toFixed(2)),
+          reach.properties.gage_0_unit,
+        ].join(" ");
       }
-      return ''
+      return "";
     },
-    viewRiver (id, tab) {
-      this.$router
-        .push(`/river-detail/${id}/${tab || 'main'}`)
-        .catch(() => {})
+    viewRiver(id, tab) {
+      this.$router.push(`/river-detail/${id}/${tab || "main"}`).catch(() => {});
     },
-    mouseoverFeature (feature) {
-      this.$store.dispatch('RiverIndex/mouseOverFeature', feature)
+    mouseoverFeature(feature) {
+      this.$store.dispatch("RiverIndex/mouseOverFeature", feature);
     },
-    centerReach (reach) {
-      this.$emit('centerReach', reach)
+    centerReach(reach) {
+      this.$emit("centerReach", reach);
     },
     // the search endpoint returns a very different looking object
     // than the tileserver, but for all the map centering logic (and table display logic)
     // to work, we need to convert the data here
-    convertSearchResponseToGeoJSON (reach) {
-      let geom
+    convertSearchResponseToGeoJSON(reach) {
+      let geom;
       if (reach.geom) {
-        const coords = reach.geom.split(',').map(x => x.split(' ').map(y => parseFloat(y)))
-        geom = lineString(coords).geometry
+        const coords = reach.geom
+          .split(",")
+          .map((x) => x.split(" ").map((y) => parseFloat(y)));
+        geom = lineString(coords).geometry;
       }
-      const readingSummaryProps = {}
+      const readingSummaryProps = {};
       if (reach.readingsummary) {
-        readingSummaryProps.gage_0_id = reach.readingsummary.gauge_id
-        readingSummaryProps.gage_0_reading = parseFloat(reach.readingsummary.gauge_reading)
-        readingSummaryProps.gage_0_updated = reach.readingsummary.updated
+        readingSummaryProps.gage_0_id = reach.readingsummary.gauge_id;
+        readingSummaryProps.gage_0_reading = parseFloat(
+          reach.readingsummary.gauge_reading
+        );
+        readingSummaryProps.gage_0_updated = reach.readingsummary.updated;
         if (reach.readingsummary.metric) {
-          readingSummaryProps.gage_0_unit = reach.readingsummary.metric.unit
+          readingSummaryProps.gage_0_unit = reach.readingsummary.metric.unit;
         }
         if (reach.readingsummary.reading) {
-          const reading = reach.readingsummary.reading
+          const reading = reach.readingsummary.reading;
           if (reading > 1) {
-            readingSummaryProps.condition = 'hi'
+            readingSummaryProps.condition = "hi";
           } else if (reading < 0) {
-            readingSummaryProps.condition = 'low'
+            readingSummaryProps.condition = "low";
           } else {
-            readingSummaryProps.condition = 'med'
+            readingSummaryProps.condition = "med";
           }
         }
       }
 
       if (!readingSummaryProps.condition) {
-        readingSummaryProps.condition = 'unk'
+        readingSummaryProps.condition = "unk";
       }
 
       return {
-        type: 'Feature',
+        type: "Feature",
         id: undefined,
         geometry: geom,
         properties: {
@@ -234,23 +240,23 @@ export default {
           altname: reach.altname,
           abstract: reach.abstract,
           ...readingSummaryProps,
-          gage_0_name: reach.gauge && reach.gauge.name
-        }
-      }
-    }
+          gage_0_name: reach.gauge && reach.gauge.name,
+        },
+      };
+    },
   },
-  created () {
-    this.debouncedMouseover = debounce(this.mouseoverFeature, 200)
+  created() {
+    this.debouncedMouseover = debounce(this.mouseoverFeature, 200);
   },
-  mounted () {
-    this.windowWidth = window.innerWidth
+  mounted() {
+    this.windowWidth = window.innerWidth;
     this.$nextTick(() => {
-      window.addEventListener('resize', () => {
-        this.windowWidth = window.innerWidth
-      })
-    })
-  }
-}
+      window.addEventListener("resize", () => {
+        this.windowWidth = window.innerWidth;
+      });
+    });
+  },
+};
 </script>
 
 <style lang="scss">
