3993f7a02b5be71056d35ec1e592d830a73d3641
Nick Gottlieb <ngottlieb@gmail.com>
2022-09-01 11:27:20 -0700

    query all reports for assign to report modal (instead of paged existing report query)

---
+++ src/app/services/getAllReachReports.js
@@ -0,0 +1,35 @@
+import http from "@/app/http"
+
+// returns a list of report titles and whether or not the user can edit them
+export async function getAllReachReports(id) {
+
+  return http
+    .post('graphql', {
+      query: `
+        query {
+            posts(
+              reach_id: "${id}",
+              first: 2500,
+              page: 1,
+              post_types:[JOURNAL],
+              orderBy: {field: POST_DATE, order: DESC}
+              ) {
+                 data {
+                    id
+                    title
+                    post_date
+                    permissions {
+                      domain
+                      permission
+                      result
+                    }
+                    user {
+                      name
+                    }
+                  }
+            }
+          }
+    `
+    })
+    .then(res => res.data)
+}
