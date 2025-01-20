c914489215110854dc20bfe95fc5297a14cfd8c2
Nick Gottlieb <ngottlieb@gmail.com>
2025-01-20 11:34:00 -0800

    remove unused reach fields and code

--- src/app/services/getReach.js
+++ src/app/services/getReach.js
@@ -13,7 +13,6 @@ export async function getReach(id) {
                   class
                   description
                   edited
-                  gaugeinfo
                   length
                   maxgradient
                   plat
