e6a7fc3dc23e318afb5aaa1419053d383b5ceba0
Nick Gottlieb <ngottlieb@gmail.com>
2026-02-05 09:41:54 -0800

    fix jenkins deploy to not build on prod server

--- server_conf/jenkins/deploy.sh
+++ server_conf/jenkins/deploy.sh
@@ -5,13 +5,14 @@ git pull origin $branch
 npm install
 export NODE_OPTIONS=--max_old_space_size=4096 
 
+## NO LONGER NEEDED
 # do the Laravel build and deploy
-npm run build
-rm -rf /var/www/americanwhitewater.org/public/prev-vue-release
-mv /var/www/americanwhitewater.org/public/static /var/www/americanwhitewater.org/public/prev-vue-release
-mv dist /var/www/americanwhitewater.org/public/static
+# npm run build
+# rm -rf /var/www/americanwhitewater.org/public/prev-vue-release
+# mv /var/www/americanwhitewater.org/public/static /var/www/americanwhitewater.org/public/prev-vue-release
+# mv dist /var/www/americanwhitewater.org/public/static
 
-# now build in s3 mode for the modern build that bypasses Laravel
+# build in s3 mode for the modern build that bypasses Laravel
 if [[ "$JENKINS_ENV" = "beta" ]]; then
   bucket="nwi-standalone-assets-beta"
   npm run builds3beta
