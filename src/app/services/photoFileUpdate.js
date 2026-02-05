acbd4f5017f9fc33f534c3007b69daa454dbfd6a
Nick Gottlieb <ngottlieb@gmail.com>
2026-02-05 09:52:02 -0800

    another pass at the file upload without apollo

--- src/app/services/photoFileUpdate.js
+++ src/app/services/photoFileUpdate.js
@@ -5,35 +5,52 @@ export async function photoFileUpdate(data) {
   const photo = Object.assign({}, data.photo);
   delete photo.id;
   const fileinput = Object.assign({}, data.fileinput);
+  const file = fileinput.file;
 
-  return laravelClient
-    .post("/graphql", {
-      query: `
-        mutation ($fileinput: PhotoFileInput!, $id:ID!, $photo: PhotoInput!) {
-          photo: photoFileUpdate(fileinput: $fileinput, id: $id, photo:$photo) {
-            id
-            caption
-            post_id
-            description
-            subject
-            photo_date
-            author
-            poi_name
-            poi_id
-            image {
-              ext
-              uri {
-                thumb
-                medium
-                big
-              }
+  // Prepare the operations object with file replaced by null
+  const operations = {
+    query: `
+      mutation ($fileinput: PhotoFileInput!, $id:ID!, $photo: PhotoInput!) {
+        photo: photoFileUpdate(fileinput: $fileinput, id: $id, photo:$photo) {
+          id
+          caption
+          post_id
+          description
+          subject
+          photo_date
+          author
+          poi_name
+          poi_id
+          image {
+            ext
+            uri {
+              thumb
+              medium
+              big
             }
           }
-        }`,
-      variables: {
-        id: id,
-        photo: photo,
-        fileinput: fileinput
+        }
+      }`,
+    variables: {
+      id: id,
+      photo: photo,
+      fileinput: {
+        ...fileinput,
+        file: null
+      }
+    }
+  };
+
+  // Create FormData for multipart request
+  const formData = new FormData();
+  formData.append('operations', JSON.stringify(operations));
+  formData.append('map', JSON.stringify({ "0": ["variables.fileinput.file"] }));
+  formData.append('0', file);
+
+  return laravelClient
+    .post("/graphql", formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
       }
     })
     .then((response) => response.data.photo);
